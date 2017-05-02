4c9e1692c4c954389bad2adae287c02b891c3fc7
delguoqing <delguoqing@hotmail.com>
2017-05-02 17:32:46 +0800

    Add parser for .mot file

---
+++ src/mot_parser.py
@@ -0,0 +1,75 @@
+import sys
+import os
+import numpy
+import util
+import mot_structs
+
+def parse(mot):
+	fourcc = mot.get("4s")
+	assert fourcc == "mot\x00"
+	version = mot.get("I")
+	assert version == 0x20120405
+	unk0 = mot.get("I")
+	track_offset = mot.get("I")
+	track_count = mot.get("I")
+	unk1 = mot.get("I")
+	name = mot.get("20s").rstrip("\x00")
+	print "MOT header: 0x%x, %d, %s" % (unk0, unk1, name)
+	tracks = read_track(mot, track_offset, track_count)
+	
+def read_track(mot, offset, count):
+	# read tracks
+	tracks = []
+	data_offsets = []
+	for trk_idx in xrange(count):
+		mot.seek(offset + trk_idx * 0xc)
+		track = mot_structs.Track()
+		track.read(mot)
+		track.adjust_offset(offset + trk_idx * 0xc)
+		tracks.append(track)
+		if track.offset > 0:
+			data_offsets.append(track.offset)
+	dummy_track = mot_structs.Track()
+	dummy_track.read(mot)
+	dummy_track.adjust_offset(offset + count * 0xc)
+	
+	# calculate track chunk data
+	data_offsets.append(mot.size)
+	data_offsets.sort()
+	offset_size = {}
+	for i in xrange(len(data_offsets) - 1):
+		data_size = data_offsets[i + 1] - data_offsets[i]
+		offset_size[data_offsets[i]] = data_size
+	
+	for trk_idx in xrange(count):
+		track = tracks[trk_idx]
+		print "Track %d @ 0x%x" % (trk_idx, offset + trk_idx * 0xc), track,
+		if track.offset > 0:
+			size = offset_size[track.offset]
+			print "0x%x" % size
+			if track.comtype in (6, 7):
+				assert size == 0xc + track.keycount * 0x4
+			elif track.comtype in (3, ):
+				pass
+			else:
+				assert False, "unknown type %d" % (track.comtype)
+		else:
+			print
+	print "Dummy Track @ 0x%x" % (mot.offset - 0xc,),  dummy_track
+	
+	for trk_idx in xrange(count):
+		track = tracks[trk_idx]
+		if track.offset:
+			print "-----------------Track %d @ 0x%x:" % (trk_idx, track.offset)
+			track.parse_keyframes(mot)
+	return tracks
+
+def parse_file(filepath):
+	fp = open(filepath, "rb")
+	mot = util.get_getter(fp, "<")
+	mot = parse(mot)
+	fp.close()
+	return mot
+
+if __name__ == "__main__":
+	parse_file(sys.argv[1])
---
+++ src/mot_structs.py
@@ -0,0 +1,43 @@
+import numpy
+
+class MOT(object):
+	pass
+
+class Track(object):
+	def read(self, mot):
+		self.bone_id = mot.get("h")
+		self.type = mot.get("B") # 0-POSX 1-POSY 2-POSZ 3-ROTX 4-ROTY 5-ROTZ
+		self.comtype = mot.get("B")	# compress type
+		self.keycount = mot.get("I")	# keyframe count
+		if self.comtype == self.keycount == 0:
+			self.const = mot.get("f")
+			self.is_const = True
+			self.offset = 0x0
+		else:
+			self.offset = mot.get("I")	# whence=os.SEEK_CUR
+			self.const = 0.0
+			self.is_const = False
+	
+	def adjust_offset(self, offset):
+		if self.offset > 0:
+			self.offset += offset
+			
+	def parse_keyframes(self, mot):
+		if self.offset <= 0:
+			return
+		mot.seek(self.offset)
+		if self.comtype == 7:
+			mot.seek(self.offset + 0xc)
+			for i in xrange(self.keycount):
+				print "key%d" % i, mot.get("4B")
+	
+	def __str__(self):
+		ret = "Bone:%d, type=%d, compress=%d, keynum=%d, " % (
+			self.bone_id, self.type, self.comtype, self.keycount)
+		if self.is_const:
+			ret += "value = %f" % self.const
+		else:
+			ret += "offset = 0x%x" % self.offset
+		return ret
+	
+
