b8508c02b03d39fe489356391da9fa0eaf71b875
delguoqing <delguoqing@hotmail.com>
2017-09-26 23:51:46 +0800

    add block data order definition

---
+++ src/const.py
@@ -0,0 +1,18 @@
+# Block Info order:
+# Bone, Unk1, Geo, SubMesh, Lod, Unk2, BoneMap, BoneSet, Mat, MeshGroup, MeshGrpMat, Unk3
+# Corresponding Block Data order:
+# Bone, Unk1, Geo, SubMesh, Lod, MeshGrpMat, Unk2, Boneset, BoneMap, MeshGroup, Mat, Unk3
+WMB_BLOCK_ORDER = [
+	0,	# Bone		
+	1,	# Unk1
+	2,	# Geo		
+	3,	# SubMesh
+	4,	# Lod
+	10,	# Unk2
+	5,	# BoneMap
+	7,	# BoneSet
+	6,	# Mat
+	9,	# MeshGroup
+	8,	# MeshGrpMat
+	11,	# Unk3
+]
