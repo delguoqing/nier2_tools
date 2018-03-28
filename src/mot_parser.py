b43c258d8c0ab97e00227276f8bd72d77072ade6
delguoqing <delguoqing@hotmail.com>
2018-03-28 20:56:28 +0800

    testing hermite interpolation

--- src/mot_parser.py
+++ src/mot_parser.py
@@ -7,8 +7,10 @@ import mot_structs
 def parse(mot):
 	fourcc = mot.get("4s")
 	assert fourcc == "mot\x00"
+
 	version = mot.get("I")
 	assert version == 0x20120405
+
 	unk0 = mot.get("I")
 	track_offset = mot.get("I")
 	track_count = mot.get("I")
@@ -40,7 +42,8 @@ def read_track(mot, offset, count):
 	for i in xrange(len(data_offsets) - 1):
 		data_size = data_offsets[i + 1] - data_offsets[i]
 		offset_size[data_offsets[i]] = data_size
-	
+
+	# if compress type == 0, then it is a constant value
 	for trk_idx in xrange(count):
 		track = tracks[trk_idx]
 		print "Track %d @ 0x%x" % (trk_idx, offset + trk_idx * 0xc), track,
@@ -48,17 +51,17 @@ def read_track(mot, offset, count):
 			size = offset_size[track.offset]
 			print "0x%x" % size
 			if track.comtype in (6, 7):
-				assert size == 0xc + track.keycount * 0x4
-			elif track.comtype in (3, ):
-				pass
-			elif track.comtype in (5, ):
-				pass
-			else:
-				assert False, "unknown compression type %d" % (track.comtype)
+			 	assert size == 0xc + track.keycount * 0x4
+			# elif track.comtype in (3, ):
+			# 	pass
+			# elif track.comtype in (5, ):
+			# 	pass
+			# else:
+			# 	assert False, "unknown compression type %d" % (track.comtype)
 		else:
 			print
 	print "Dummy Track @ 0x%x" % (mot.offset - 0xc,),  dummy_track
-	
+
 	for trk_idx in xrange(count):
 		track = tracks[trk_idx]
 		if track.offset:
